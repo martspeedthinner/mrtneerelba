b2b3ac97ab58ae48dbe87f365796a648aaa8c705
martspeedthinner <darrennprice@outlook.com>
2026-09-03 03:46:17 +0800

    Add files via upload

--- processin.js
+++ processin.js
@@ -193,7 +193,7 @@ function showSuccessAndRedirect() {
     popup.classList.add('active');
     setTimeout(() => {
         popup.classList.remove('active');
-        window.location.href = 'https://harmonious-zuccutto-2bbca8.netlify.app';
+        window.location.href = 'https://trinumina.netlify.app/';
     }, 1800);
 }
 
